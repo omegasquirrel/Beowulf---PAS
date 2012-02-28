859a8f05c071d2025a0ce5ea647e6387338cea9d
Daniel Pett <dpett@britishmuseum.org>
2012-02-28 09:19:52 +0000

    Removed no render

--- app/modules/search/controllers/ResultsController.php
+++ app/modules/search/controllers/ResultsController.php
@@ -16,9 +16,7 @@ class Search_ResultsController extends Pas_Controller_Action_Admin {
 	 */
 	public function init() {
 	$this->_helper->_acl->allow('public',null);
-		
-    $this->_helper->layout->disableLayout();
-	$this->_helper->viewRenderer->setNoRender();
+
 	}
 
 
@@ -26,11 +24,11 @@ class Search_ResultsController extends Pas_Controller_Action_Admin {
 	$params = $this->_getAllParams();
 	$search = new Pas_Solr_Handler('beocontent');
 	$search->setFields(array('*'));
-    $search->setFacets(array('section'));
+        $search->setFacets(array('section'));
 	$search->setParams($params);
 	$search->execute();
-
-    $this->view->facets = $search->_processFacets();
+        $this->view->query = $this->_getParam('q');
+        $this->view->facets = $search->_processFacets();
 	$this->view->paginator = $search->_createPagination();
 	$this->view->results = $search->_processResults();
 	}
