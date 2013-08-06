973a81ea18d4c78ad1546235d5649aebd8c51655
portableant <dpett@britishmuseum.org>
2013-08-06 11:56:45 +0100

    Rally pages updated for google maps version 3. This fixes #63

--- library/Pas/View/Helper/LatestRecords.php
+++ library/Pas/View/Helper/LatestRecords.php
@@ -83,7 +83,7 @@ class Pas_View_Helper_LatestRecords extends Zend_View_Helper_Abstract{
 
 
 	public function buildHtml($data){
-	if($data['images']) {
+	if(array_key_exists('images', $data)) {
 	$html = '<h3>Latest examples recorded with images</h3>';
 	$html .= '<p>We have recorded ' . number_format($data['numberFound'])
                 . ' examples.</p>';
